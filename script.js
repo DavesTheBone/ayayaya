1293a20e89cf8dbdb249463b1585d53a92e28e41
Daves The Bone <davesthebone@gmail.com>
2024-10-02 20:19:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 window.onload = function() {
     const audio = document.getElementById('audio');
-    audio.play();
+    // ページがロードされたときに音楽を再生
+    audio.play().catch(error => {
+        console.log('Music playback is blocked, like seriously? Let the user play it already, you clueless fucking　idiot');
+    });
 };
